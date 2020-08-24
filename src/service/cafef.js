58fda13ea3fbdd2c3cf8bc8758eecb48d7f26a48
dungdev <dung.truong@kompa.ai>
2020-08-24 20:51:36 +0700

    add createdDate

--- src/service/cafef.js
+++ src/service/cafef.js
@@ -17,6 +17,11 @@ const pushData = async (data) => {
   sendToQueue({ ...data });
 };
 
+const getLiTags = (cheerioStatic) => {
+  const bodyContent = cheerioStatic('body');
+  return bodyContent.find('.tlitem');
+};
+
 const getATags = (cheerioStatic) => {
   const bodyContent = cheerioStatic('body');
   return bodyContent.find('a');
@@ -51,19 +56,21 @@ const cafefSubPageCrawler = (domain) => {
     fetchData(originLink).then((res) => {
       const html = res.data;
       const cheerioStatic = cheerio.load(html);
-      const aTags = getATags(cheerioStatic);
-      aTags.each(function () {
-        const title = cheerioStatic(this).text();
+      const liTags = getLiTags(cheerioStatic);
+      liTags.each(function () {
+        const aTag = cheerioStatic(this).find('.avatar');
+        const title = aTag.attr('title');
         if (title) {
           config.keywords.forEach((word) => {
             const matched = title.match(word);
             if (matched) {
-              const dateAttr = cheerioStatic(this).parent().parent().attr('data-newsid');
+              const dateAttr = cheerioStatic(this).attr('data-newsid');
               const createdDate = convertStrToCreatedDate(dateAttr);
-              const shortDescription = cheerioStatic(this).find('a').text();
-              const link = cheerioStatic(this).attr('href');
+              const shortDescription = cheerioStatic(this).find('.knswli-right').find('.sapo').text();
+              const link = aTag.attr('href');
+              const image = aTag.find('img').attr('src');
               // Handle push data to message queue
-              pushData({ link: `${homepageLink}${link}`, title, shortDescription, originLink, createdDate });
+              pushData({ link: `${homepageLink}${link}`, image, title, shortDescription, originLink, createdDate });
             }
           });
         }
---
+++ src/service/ndh.js
@@ -0,0 +1,40 @@
+/* eslint-disable camelcase */
+import { format } from 'date-fns';
+import config from '../config';
+import { fetchData } from '../util/request';
+import { sendToQueue } from '../util/queue';
+
+const NDH_CHUNGKHOAN_CATE = '1000527';
+const NDH_DOANHNGHIEP_CATE = '1000595';
+
+const originLink = 'https://ndh.vn/lazyload-more';
+
+const pushData = async (data) => {
+  sendToQueue({ ...data });
+};
+
+const ndhNews = async (cateId) => {
+  for (let i = 0; i < 10; i++) {
+    const news = await fetchData(originLink, {
+      params: { data: { page: i, cate_id: cateId } }
+    });
+    const { data: { data: newsData } } = news;
+    newsData.forEach((newData) => {
+      const { title, share_url, lead, thumbnail_url, publish_time } = newData;
+      const matched = config.keywords.find((keyword) => !!title.match(keyword));
+      if (matched) {
+        pushData({
+          link: `${originLink}${share_url}`,
+          createdDate: format(new Date(publish_time * 1000), 'yyyy-MM-dd'),
+          title,
+          shortDescription: lead,
+          originLink,
+          image: thumbnail_url
+        });
+      }
+    });
+  }
+};
+
+ndhNews(NDH_DOANHNGHIEP_CATE);
+ndhNews(NDH_CHUNGKHOAN_CATE);
