bee356c2d9776e983b96de1a5d6d0496c007a465
dungdev <dung.truong@kompa.ai>
2020-08-24 20:51:36 +0700

    add createdDate

--- src/service/cafef.js
+++ src/service/cafef.js
@@ -4,6 +4,15 @@ import config from '../config';
 import { fetchData } from '../util/request';
 import { sendToQueue } from '../util/queue';
 
+const homepageLink = `${config.cafefDomain}`;
+
+const convertStrToCreatedDate = (data) => {
+  const year = data.substring(0, 4);
+  const month = data.substring(4, 6);
+  const day = data.substring(6, 8);
+  return `${year}-${month}-${day}`;
+};
+
 const pushData = async (data) => {
   sendToQueue({ ...data });
 };
@@ -15,8 +24,7 @@ const getATags = (cheerioStatic) => {
 
 const cafefHompagePage = () => {
   // Crawl from page 1 to 4.
-  const originLink = `${config.cafefDomain}`;
-  fetchData(originLink).then((res) => {
+  fetchData(homepageLink).then((res) => {
     const html = res.data;
     const cheerioStatic = cheerio.load(html);
     const aTags = getATags(cheerioStatic);
@@ -26,10 +34,10 @@ const cafefHompagePage = () => {
         config.keywords.forEach((word) => {
           const matched = title.match(word);
           if (matched) {
-            const shortDescription = cheerioStatic(this).find('a').text();
             const link = cheerioStatic(this).attr('href');
+            const shortDescription = cheerioStatic(this).find('a').text();
             // Handle push data to message queue
-            pushData({ link, title, shortDescription, originLink });
+            pushData({ link: `${homepageLink}${link}`, title, shortDescription, originLink: homepageLink });
           }
         });
       }
@@ -38,7 +46,7 @@ const cafefHompagePage = () => {
 };
 
 const cafefSubPageCrawler = (domain) => {
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 20; i++) {
     const originLink = `${domain}trang-${i + 1}.chn`;
     fetchData(originLink).then((res) => {
       const html = res.data;
@@ -50,10 +58,12 @@ const cafefSubPageCrawler = (domain) => {
           config.keywords.forEach((word) => {
             const matched = title.match(word);
             if (matched) {
+              const dateAttr = cheerioStatic(this).parent().parent().attr('data-newsid');
+              const createdDate = convertStrToCreatedDate(dateAttr);
               const shortDescription = cheerioStatic(this).find('a').text();
-              const link = cheerioStatic(this).find('a').attr('href');
+              const link = cheerioStatic(this).attr('href');
               // Handle push data to message queue
-              pushData({ link, title, shortDescription, originLink });
+              pushData({ link: `${homepageLink}${link}`, title, shortDescription, originLink, createdDate });
             }
           });
         }
