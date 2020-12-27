317760f864d1d7df5afd5807d2a51cb9e25f6501
dungdev <dung.truong@kompa.ai>
2020-12-27 10:42:14 +0700

    update crawler

--- src/service/vietnambiz.js
+++ src/service/vietnambiz.js
@@ -8,9 +8,9 @@ const pushData = async (data) => {
   sendToQueue({ ...data });
 };
 
-const getNewstreamDiv = (cheerioStatic) => {
+const getNewsOnTopPage = (cheerioStatic) => {
   const bodyContent = cheerioStatic('body');
-  return bodyContent.find('.w510 .news-stream');
+  return bodyContent.find('.w510.fl');
 };
 
 export const vietnambizChungKhoan = (apiLink) => {
@@ -19,35 +19,34 @@ export const vietnambizChungKhoan = (apiLink) => {
     fetchData(`${originLink}/${apiLink}/trang-${i}.htm`).then((res) => {
       const html = res.data;
       const cheerioStatic = cheerio.load(html);
-      const newstreams = getNewstreamDiv(cheerioStatic);
-      newstreams.each(function () {
-        const listNews = cheerioStatic(this).find('.listnews li');
-        listNews.each(() => {
-          const image = cheerioStatic(this).find('img');
-          const description = cheerioStatic(this).find('.description');
-          const title = image.attr('title');
-          if (title) {
-            config.keywords.forEach((word) => {
-              const matched = title.match(new RegExp(word, 'i'));
-              if (matched) {
-                const link = `${originLink}${description.find('a').attr('href')}`;
-                const imageLink = image.attr('src');
-                const shortDescription = description.find('.sapo').text();
-                const date = description.find('.need-get-timeago').attr('title');
-                // Handle push data to message queue
-                pushData({
-                  link,
-                  createdDate: new Date(date),
-                  title,
-                  shortDescription,
-                  originLink,
-                  image: imageLink,
-                  publishedDate: new Date()
-                });
-              }
-            });
-          }
-        });
+      const newsOnTopPage = getNewsOnTopPage(cheerioStatic);
+      const newstreams = newsOnTopPage.find('.news-stream');
+      const listNews = newstreams.find('.listnews li');
+      listNews.each(function () {
+        const image = cheerioStatic(this).find('img');
+        const description = cheerioStatic(this).find('.description');
+        const title = description.find('h3.title a').text();
+        if (title) {
+          config.keywords.forEach((word) => {
+            const matched = title.match(new RegExp(word, 'i'));
+            if (matched) {
+              const link = `${originLink}${description.find('a').attr('href')}`;
+              const imageLink = image.attr('src');
+              const shortDescription = description.find('.sapo').text();
+              const date = description.find('.need-get-timeago').attr('title');
+              // Handle push data to message queue
+              pushData({
+                link,
+                createdDate: new Date(date),
+                title,
+                shortDescription,
+                originLink,
+                image: imageLink,
+                publishedDate: new Date()
+              });
+            }
+          });
+        }
       });
     });
   }
